2e337a16c247b6b1be1484da80941a5ffd13d5c1
Daniel Priestley <dannypriestley3@gmail.com>
2023-10-31 13:00:28 -0400

    Initial Commit

---
+++ MiniFlickr/MiniFlickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniFlickrApp.swift
+//  MiniFlickr
+//
+//  Created by Daniel Priestley on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MiniFlickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
